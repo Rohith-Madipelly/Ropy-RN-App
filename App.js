fea114715ea28d458fbda780b120e9c7a76ddad3
Rohith Madipelly <madipellyrohith@gmail.com>
2024-10-17 11:30:17 +0530

    Updateing all my code to git on 17-10-2024

--- App.js
+++ App.js
@@ -1,5 +1,3 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 import { Provider } from 'react-redux';
 import Screens from './app/Screens';
 import { store } from './app/redux/store';
